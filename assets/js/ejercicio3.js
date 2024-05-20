79abb386590b999a9f5fc7dfee2071d4ab54bdd4
spl-NicoValderrama <valderrama.nicolas@outlook.com>
2024-05-20 10:57:01 -0400

    commit finalización desafío

--- assets/js/ejercicio3.js
+++ assets/js/ejercicio3.js
@@ -1,12 +1,29 @@
 const inputNumber1 = document.getElementById("valor1");
-const inputNumber2 = document.getElementById("valor 2");
-const resultSpan = document.querySelector("resultado");
+const inputNumber2 = document.getElementById("valor2");
+const resultSpan = document.querySelector(".resultado");
 
 const addition = document.getElementById("btn-sumar");
-const subtraction = documento.getElementById("btn-restar");
+const subtraction = document.getElementById("btn-restar");
 
 
+// Suma
 
+function makeSum() {
+    const num1 = Number(inputNumber1.value);
+    const num2 = Number(inputNumber2.value);
+    const sum = num1 + num2;
+    resultSpan.innerHTML = sum
+}
+
+// resta
+
+function doSubtraction() {
+    const num1 = Number(inputNumber1.value);
+    const num2 = Number(inputNumber2.value);
+    let subtract = num1 - num2;
+    if (subtract < 0) subtract = 0;
+    resultSpan.innerHTML = subtract
+}
 
 addition.addEventListener("click", makeSum);
 subtraction.addEventListener("click", doSubtraction);
